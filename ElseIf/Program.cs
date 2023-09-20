783f8b949a0680daaf1a3a9d864b888c4aca32fe
GustavoGBarros <gustavogbarros01@gmail.com>
2023-09-20 04:18:06 -0300

    Add project files.

---
+++ ElseIf/Program.cs
@@ -0,0 +1,28 @@
+﻿
+do
+{
+    while (!Console.KeyAvailable)
+    {
+
+        string letra = "";
+
+        Console.WriteLine();
+        Console.WriteLine("Digite uma letra (A, B ou C)");
+       
+        letra = Console.ReadLine();
+
+        if (letra.ToLower() == "a")
+        {
+            Console.WriteLine("Você escolheu a letra A");
+        }
+        else if (letra.ToLower() == "b")
+        {
+            Console.WriteLine("Você escolheu a letra B");
+        }
+        else
+        {
+            Console.WriteLine("Você escolheu a letra C");
+        }
+
+    }
+} while (Console.ReadKey().Key == ConsoleKey.Escape);
---
+++ LeitorNome/Program.cs
@@ -0,0 +1,31 @@
+﻿string nome = "";
+short idade = 0;
+const double tempc = 36.5;
+
+Console.WriteLine("Escreva seu nome");
+
+nome = Console.ReadLine();
+
+Console.WriteLine("Digite a sua idade");
+
+try
+{
+    idade = Convert.ToInt16(Console.ReadLine());
+    if (idade < 0 || idade > 160)
+    {
+        throw new ArgumentOutOfRangeException();
+    }
+    //if (idade is < 0 or > 150)
+    //{
+    //    Console.WriteLine("Digite uma idade válida");
+    //}
+    Console.WriteLine($"O seu nome é {nome} e a sua idade é {idade} e a sua temperatura corporal normal é {tempc} ");
+}
+catch (ArgumentOutOfRangeException)
+{
+    Console.WriteLine("Digite uma idade válida");
+}
+catch (FormatException)
+{
+    Console.WriteLine("Digite um número válido");
+}
